56c99b1c3d85bfb59267130bbdaafa1d9c542e63
JonHarris <jonathan.harris.eit@gmail.com>
2021-02-25 15:53:30 -0500

    add interview_cake folder

--- HackerRank/Python/regex_notes.py
+++ HackerRank/Python/regex_notes.py
@@ -7,6 +7,8 @@ re.search(r'pattern',str)  # check if pattern present, return match obj
 				  'This is a sample string', # input string
 				  flags=re.I)                # ignore case
 		Output = <re.Match object; span=(2, 4), match='is'>	
+	output.start() # start index
+	output.end()   # end index
 
 re.findall(r'pattern',str) # return list of string matching pattern
 	# Example: 
@@ -27,9 +29,29 @@ r'\bpattern\b' # restricts match to start/end of words
 # Character Class
 r'\s' # match whitespace chars [\ \t\n\r\f\v]
 r'\S' # match non-whitespacer chars
-
 r'\d' # match digits [0-9]
 r'\D' # match non-digits [^0-9] or [^\d]
-
 r'\w' # match word chars [0-9a-zA-Z_]
 r'\W' # match non-word chars
+
+
+
+# Create Groups
+r'(pattern)' 	# define group looking to match
+	# () Example
+			pattern = '(\d+|\w+)' # find words or numbers
+			re.findall(pattern, 'He has 11 cats')
+			# output = ['He', 'has', '11', 'cats']
+r'[abc]' # match ONLY ONE out of characters within square brackets
+	#Ex: '^[1-3][0-2][xs0][30Aa][xsu][.,]'
+		# i=0: 1,2, or 3
+		# i=1: 0,1, or 2
+		# i=2: x, s, or 0
+		# etc...
+r'[^abc]' # match if chars NOT within square brackets
+r'pattern{x}' # match if pattern exactly x repetitions
+
+
+
+
+	
